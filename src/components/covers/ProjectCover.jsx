db9eb4b3d8c81586718fc319a968d90a4a5ec44e
k9opAA <engr.aliazgor@gmail.com>
2026-05-06 10:04:38 +0600

    PR

--- src/components/covers/ProjectCover.jsx
+++ src/components/covers/ProjectCover.jsx
@@ -117,7 +117,7 @@ const ProjectCover = ({
             </div>
 
             {/* Project Title */}
-            <div className="project-title">PROJECT REPORTS</div>
+            <div className="project-title">PROPOSAL REPORT</div>
 
             {/* Course Info Box - Centered */}
             <div className="course-info-box">
